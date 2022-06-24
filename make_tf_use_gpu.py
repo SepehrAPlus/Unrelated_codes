5c93498fea9e5a3ad77f39fde25af249823cc1cf
who-rly-cares <88319573+who-rly-cares@users.noreply.github.com>
2022-06-25 01:48:45 +0430

    just wrote it

---
+++ make_tf_use_gpu.py
@@ -0,0 +1,49 @@
+import os
+import platform
+
+os_name_allowed_filter = lambda n : n == "Windows"
+lib_to_remove = "tensorflow"
+libs_to_install = ["tensorflow-gpu","pycuda","pip install nvidia-cudnn"]
+uninsatall_command = 'pip uninstall "{lib_name}"'
+install_command = 'pip install "{lib_name}"'
+cmd_sender = os.popen
+
+def uninstall_lib(lib_name):
+    output = cmd_sender(uninsatall_command.format(lib_name=lib_name)).read()
+    print(f"[CMD] : \n {output} \n {'='*15}")
+
+
+def install_lib(lib_name):
+    output = cmd_sender(install_command.format(lib_name=lib_name)).read()
+    print(f"[CMD] : \n {output} \n {'='*15}")
+
+
+def get_current_os_name():
+    return platform.system()
+
+
+def avoid_execution_on_non_windows():
+    os_name = get_current_os_name()
+    if os_name_allowed_filter(os_name) == False:
+        print("[ERROR]: THIS CODE CAN ONLY BE USED ON WINDOWS")
+    else:
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+if '__main__' == __name__:
+    print("@Who_cares_anyway")
+    avoid_execution_on_non_windows()
+    uninstall_lib(lib_to_remove)
+    for i1 in libs_to_install:
+        install_lib(i1)

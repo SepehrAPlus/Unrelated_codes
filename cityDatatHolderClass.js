0c6b149f20e3d358783691bce466bd9808bda37a
who-rly-cares <88319573+who-rly-cares@users.noreply.github.com>
2023-02-01 13:23:57 +0330

    Update cityDatatHolderClass.js

--- cityDatatHolderClass.js
+++ cityDatatHolderClass.js
@@ -1,3 +1,4 @@
+//اینو بزار اول کدت
 class MainCityDataHolder {
  	constructor(city_data_object){
     	this.city_data_object = city_data_object;
@@ -13,9 +14,11 @@ class MainCityDataHolder {
     }
 }
 
-
-
+//اینو  بزار جای خط 11 تا 14
 let IranCityData = new MainCityDataHolder(
   {tehran : "data1",yazd : "data2",})
 
+
+//اینوطوریم ازش داده شهر رو بگیر 
+// اگر شهر وجود ندشته باشه میزنه "NO_DATA"
 console.log(IranCityData.get_city_data("kerman"))
